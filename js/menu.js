e3f44cabe0dab825459b0c834e85496ff76a20ec
Mavegaji <marcovega.jz@gmail.com>
2025-05-18 22:10:43 -0600

    release 3

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+<header class="main-header container">
+  <img src="logo.png" alt="Logo" class="logo" />
+  <nav>
+    <ul class="nav-links" id="nav-links">
+      <li><a href="#" aria-current="page">Home</a></li>
+      <li><a href="#">Features</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <div class="menu-toggle" id="menu-toggle" aria-label="Toggle menu" role="button" tabindex="0">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </nav>
+</header>
